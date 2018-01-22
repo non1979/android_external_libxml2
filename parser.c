c5a9605c418094eacabd91795076719138d07ba4
Nick Wellnhofer <wellnhofer@aevum.de>
2018-01-22 15:40:05 +0100

    Halt parser in case of encoding error
    Should fix crbug.com/793715, although I wasn't able to reproduce the
    issue.

--- parser.c
+++ parser.c
@@ -12269,6 +12269,7 @@ xmldecl_done:
 		    /* TODO 2.6.0 */
 		    xmlGenericError(xmlGenericErrorContext,
 				    "xmlParseChunk: encoder error\n");
+                    xmlHaltParser(ctxt);
 		    return(XML_ERR_INVALID_ENCODING);
 		}
 		xmlBufSetInputBaseCur(in->buffer, ctxt->input, base, current);
